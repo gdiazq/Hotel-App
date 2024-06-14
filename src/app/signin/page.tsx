f08ae5a6dc6278fb8d13a4933bf9c83d68ce91ea
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-14 00:16:28 -0400

    Dia 4

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+const DashboardLogin = () => {
+  return (
+    <div>
+      <h1>Dashboard Login Page!</h1>
+    </div>
+  );
+}
+
+export default DashboardLogin;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+const SettingPage = () => {
+    return (
+        <div>
+            <h1>Settings Page</h1>
+        </div>
+    );
+}
+
+export default SettingPage;
