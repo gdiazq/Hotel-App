f8a300ac35cc9686ca2250b2d52cf8b3144b1677
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-17 19:03:10 -0400

    Shadcn + NextUI

--- src/components/ui/form/FormSignIn.tsx
+++ src/components/ui/form/FormSignIn.tsx
@@ -3,25 +3,40 @@
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/shadcn/form'
+import { LoginHotelSchema } from '@/schemas/signin';
 import InputEmail from '@/components/ui/input/InputEmail';
 import InputPassword from '@/components/ui/input/InputPassword/InputPassword';
 import { LoginButton } from '@/components/ui/auth/LoginButton';
-import { LoginHotelSchema } from '@/schemas/signin';
 
 export const FormSignIn = () => {
+
     const form = useForm<z.infer<typeof LoginHotelSchema>>({
         resolver: zodResolver(LoginHotelSchema),
         defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+          email: '',
+          password: '',
+        }
+    })
+
 
     return (
-        <form action="/api/auth/username" method="post">
-            <InputEmail />
-            <InputPassword />
-            <LoginButton />
-        </form>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})}>
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <InputEmail />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                <InputPassword />
+                <LoginButton />
+            </form>
+        </Form>
     )
 };
