63956f9448b33b0ed6f86743813ad6bf98bc207b
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-17 15:55:23 -0400

    Tendre que hacer los form con shadcn

--- src/components/ui/form/FormSignIn.tsx
+++ src/components/ui/form/FormSignIn.tsx
@@ -3,20 +3,30 @@
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+ } from '@/components/ui/form';
+
 import InputEmail from '@/components/ui/input/InputEmail';
 import InputPassword from '@/components/ui/input/InputPassword/InputPassword';
 import { LoginButton } from '@/components/ui/auth/LoginButton';
 import { LoginHotelSchema } from '@/schemas/signin';
 
-export const LoginForm = () => {
+export const FormSignIn = () => {
     const form = useForm<z.infer<typeof LoginHotelSchema>>({
         resolver: zodResolver(LoginHotelSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
 
     });
-};
 
-
-export const FormSignIn = () => {
     return (
         <form action="/api/auth/username" method="post">
             <InputEmail />
