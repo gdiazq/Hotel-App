40e5272b87488eda6e70d2b93e66a1a21af408cc
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-18 03:01:40 -0400

    Dia 8

--- src/components/ui/form/FormSignUp.tsx
+++ src/components/ui/form/FormSignUp.tsx
@@ -3,18 +3,54 @@
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/shadcn/form'
 import { RegisterButton } from '@/components/ui/auth/RegisterButton';
+import { RegisterHotelSchema } from '@/schemas/signup';
 import  InputEmail from '@/components/ui/input/InputEmail';
 import InputPassword from '@/components/ui/input/InputPassword/InputPassword';
 import InputRepeatPassword from '@/components/ui/input/InputPassword/InputRepeatPassword';
 
 export const FormSignUp = () => {
+
+    const form = useForm<z.infer<typeof RegisterHotelSchema>>({
+        resolver: zodResolver(RegisterHotelSchema),
+        defaultValues: {
+          email: '',
+          password: '',
+        }
+    })
+
+
     return (
-        <form action="/api/auth/register" method="post">
-            <InputEmail />
-            <InputPassword />
-            <InputRepeatPassword />
-            <RegisterButton />
-        </form>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})}>
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <InputEmail />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <InputPassword />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <InputRepeatPassword />
+                <RegisterButton />
+            </form>
+        </Form>
     )
 };
--- src/schemas/signup/index.ts
+++ src/schemas/signup/index.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const LoginHotelSchema = z.object({
+export const RegisterHotelSchema = z.object({
     email: z.string().email({ message: 'Invalid email address' }),
     password: z.string().min(8, { message: 'Password must be at least 8 characters long'}),
     repeatpassword: z.string().min(8, { message: 'Password must be at least 8 characters long'})
