830568be0a30484df8069620e8b55689764aae2d
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-19 16:40:29 -0400

    Archivo register.ts listo

---
+++ src/actions/register.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import * as z from 'zod';
+import { RegisterHotelSchema  } from '@/schemas/signup';
+
+export const login = async (values: z.infer<typeof RegisterHotelSchema>) => {
+    const validatedFields = RegisterHotelSchema.safeParse(values);
+    if (validatedFields.success) {
+        return { success: "Register success" };
+    } else {
+        return { error: "Invalid credentials" };
+    }
+}
