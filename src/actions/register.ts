973bf746b2221c0c264c5b9e50695c32bb4d48fc
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-20 21:35:46 -0400

    Funciono con base de datos PostGreSQL

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,34 +1,28 @@
 "use server";
-import { db } from '@/lib/db';
+
+import { prisma } from '@/lib/prisma';
 import bcrypt from 'bcrypt';
 import * as z from 'zod';
 import { RegisterHotelSchema  } from '@/schemas/signup';
 
 export const register = async (values: z.infer<typeof RegisterHotelSchema>) => {
     const validatedFields = RegisterHotelSchema.safeParse(values);
-    if (validatedFields.success) {
-        const { email, password, repeatpassword } = validatedFields.data;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const hashedRepeatPassword = await bcrypt.hash(repeatpassword, 10);
-        const existingUser = await db.user.findUnique({
-            where: {
-                email
-            }
-        });
-        if (existingUser) {
-            return { error: "User already exists" };
-        }
-
-        await db.user.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword,
-                repeatpassword: hashedRepeatPassword,
-            }
-        })
-        return { success: "Register success" };
-    } else {
-        return { error: "Invalid credentials" };
+    if (!validatedFields.success) {
+        return { error: "Invalid fields!" };
     }
+
+    const { name, email, password, repeatpassword } = validatedFields.data;
+    if (password !== repeatpassword) {
+        return { error: "Passwords do not match!" };
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+    return { success: "Register success" };
 }
