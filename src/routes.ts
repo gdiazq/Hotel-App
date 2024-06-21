61b33d03ac85127f354024a72de6b29e0adbc00d
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-06-21 17:40:58 -0400

    Continuando con los configuraciones V2

--- src/routes.ts
+++ src/routes.ts
@@ -14,9 +14,22 @@ export const publicRoutes = [
  * @type {string[]}
  */
 
-export const authRouter = [
+export const authRoutes = [
     "/signin",
     "/signup",
 ]
 
+/**
+ * An array of routes that are used for the API.
+ * These routes are prefixed with /api
+ * @type {string[]}
+ */
+
 export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default route to redirect to after a successful login.
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
